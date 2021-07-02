e0258cf826275f803592e5fba1c0aa4cd9ae88d4
dwightshrude <akyolz@mef.edu.tr>
2021-07-02 09:04:31 +0300

    Third commit

--- src/components/design/Display.js
+++ src/components/design/Display.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import './Display.css';
 
 const Display = () => {
     return (
-        <h3>Display</h3>
+        <div className="card card-content">
+            <div className="imgTshirt text-center">
+                <img
+                    className="img-resposive" src="https://res.cloudinary.com/dkkgmzpqd/image/upload/v1545217305/T-shirt%20Images/black.png"
+                    alt="img-Tshirt" />
+            </div>
+            <div className="memeText text-center">
+                <div className="upperText">
+                    <p>Upper Text</p>
+                </div>
+                <img
+                    src="http://via.placeholder.com/400x300"
+                    alt="memetext"
+                />
+                <div className="lowerText">
+                    <p>Lower Text</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/design/Settings.js
+++ src/components/design/Settings.js
@@ -1,8 +1,41 @@
 import React from 'react';
+const urlImgBase = "https://res.cloudinary.com/dkkgmzpqd/image/upload/v1545217305/T-shirt%20Images/";
 
 const Settings = () => {
     return (
-        <h3>Settings</h3>
+        <div className="card bg-light container">
+            <h3 className="text-center">Settings</h3>
+            <h4>Change T-shirt Color</h4>
+            <div ClassName="tshirt-color">
+                <img src={'${urlImgBase}white.png'} alt="white-tshirt" />
+                <img src={'${urlImgBase}black.png'} alt="black-tshirt" />
+                <img src={'${urlImgBase}grey.png'} alt="grey-tshirt" />
+                <img src={'${urlImgBase}blue.png'} alt="blue-tshirt" />
+                <img src={'${urlImgBase}red.png'} alt="red-tshirt" />
+            </div>
+            <hr />
+            <h4>Write Text</h4>
+            <input type="text" className="form-control form-control-sm mb-2" placeholder="Upper Text" />
+            <input type="text" className="form-control form-control-sm" placeholder="Lower Text" />
+            <hr />
+            <h4>Upload Image</h4>
+            <div className="form-group">
+                <input type="file" className="form-control-file mb-2" />
+            </div>
+            <hr />
+            <h4>Text Size</h4>
+            <input type="range" min="0" max="100" />
+            <hr />
+            <h4>Text Color</h4>
+            <select className="form-control-sm mb-2">
+                <option>White</option>
+                <option>Black</option>
+                <option>Red</option>
+                <option>Blue</option>
+            </select>
+            <hr />
+            <button className="btn btn-primary btn-sm-mb-2">Save</button>
+        </div>
     )
 }
 
